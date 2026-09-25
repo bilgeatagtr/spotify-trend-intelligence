4ae101407b67ab41caf29fb4ca949cd079081221
Bilge Atag <atagbilge@gmail.com>
2026-09-25 12:48:42 +0300

    Allow frontend origin in CORS settings

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://jolly-field-0278d8c1e.6.azurestaticapps.net"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
